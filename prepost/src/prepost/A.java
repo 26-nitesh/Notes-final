03d70eddf2872c4fdd642b8cb5b483e65aefdc53
nkumar <nkumar@4321#>
2024-05-03 19:18:29 +0530

    'PrePost'

---
+++ prepost/src/prepost/A.java
@@ -0,0 +1,32 @@
+package prepost;
+
+public class A {
+	//Pre Increment/Decrement (Syntax => ++var/--var) => 
+// * incrementing/decrementing the
+//	the value by 1
+	public static void main(String[] args) {
+//		int n = 6;
+////		++n;// first it will increase its value and then return 
+////		the increased Value
+//		System.out.println(++n);
+		
+		int x = 8;
+		System.out.println(--x);
+	}
+	
+	
+	//Post Increment/post decrement -> increases/decreses the value by one
+//	public static void main(String[] args) {
+	
+	/*	int n = 6;
+//		post increament/decrement will first return the original value and then it 
+//		increse/decrese its value
+		System.out.println(n++);
+		System.out.println(n); */
+		
+//		
+//		int x = 7;
+//		System.out.println(x--);
+//		System.out.println(x);
+//	}
+}
---
+++ prepost/src/prepost/B.java
@@ -0,0 +1,29 @@
+package prepost;
+
+public class B {
+
+	public static void main(String[] args) {
+//		int n = 8;
+//		n++;//9
+//		--n;//8
+//		++n;//9
+//		System.out.println(n);//
+//		
+		
+//		int n = 7;
+//		int x = n++;
+//		System.out.println(x);//7
+//		System.out.println(n);//8
+		
+//		int n = 9;
+//		int x = ++n;//pre
+//		System.out.println(x);
+//		System.out.println(n);
+		
+//		int a = 77;
+//		int b = ++a+a++;
+//		System.out.println(a);
+//		System.out.println(b);
+		
+	}
+}
